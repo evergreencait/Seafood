3d77fc15190bae57fedcc6901552683e50b28a34
Caitlin Hines <caitlinhines@me.com>
2017-05-01 08:30:35 -0700

    styled edit link to be on each post when logged in

--- src/Seafood/Startup.cs
+++ src/Seafood/Startup.cs
@@ -42,10 +42,34 @@ namespace Seafood
             });
 
             app.UseStaticFiles();
-            app.Run(async (context) =>
+            var context = app.ApplicationServices.GetService<ApplicationDbContext>();
+            AddSeedData(context);
+            app.Run(async (context1) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context1.Response.WriteAsync("Hello World!");
             });
+        }
+
+        private static void AddSeedData(ApplicationDbContext context)
+        {
+            var dummyPost = new Models.Post
+            {
+                Description = "Alaskan Cod on Sale",
+                ImagePath = "https://fthmb.tqn.com/zL2MRlV_In8suQ4N1A26GBTxXFY=/768x0/filters:no_upscale()/about/Black-cod-sablefish-58b8ea473df78c353c297e37.jpg"
+
+            };
+
+            context.Posts.Add(dummyPost);
+
+            var dummyPost2 = new Models.Post
+            {
+                Description = "Lemon Baked Cod Recipe",
+                ImagePath = "http://img.sndimg.com/food/image/upload/h_420,w_560,c_fit/v1/img/recipes/13/52/72/aML6ha46QVqfQuw5Fgu5_THE%20FOOD%20GAYS%20-%20BAKED%20LEMON%20COD.jpg"
+            };
+
+            context.Posts.Add(dummyPost2);
+
+            context.SaveChanges();
         }
     }
 }
